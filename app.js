8db4b889f3c1b55b7114ec945b9f63ec37facf54
drgaandco <147396771+drgaandco@users.noreply.github.com>
2023-10-09 06:11:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_4zk987c", "template_7w4ybzb", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
